a41838fa07984816db98e2503c65b972cd601fe3
david.van.steertegem@gmail.com <Langwachtwoord1>
2020-05-18 15:28:33 +0200

    created fragment for menu created base navigation

---
+++ src/test/java/be/ehb/ects/EctsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.ects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EctsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
